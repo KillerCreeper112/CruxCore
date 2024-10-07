6b2c27d4c71ac3bdf1838e63885123316de54776
KillerCreeper <43390898+KillerCreeper112@users.noreply.github.com>
2024-10-07 17:01:19 -0400

    ye

--- src/main/java/killercreepr/cruxcore/config/handler/FileDynamicUpdater.java
+++ src/main/java/killercreepr/cruxcore/config/handler/FileDynamicUpdater.java
@@ -1,6 +1,5 @@
 package killercreepr.cruxcore.config.handler;
 
-import killercreepr.crux.item.dynamic.BukkitDynamicItem;
 import killercreepr.crux.item.dynamic.DynamicItem;
 import killercreepr.crux.item.predicate.ItemPredicate;
 import killercreepr.crux.util.CruxObjects;
@@ -25,7 +24,7 @@ public class FileDynamicUpdater implements FileObjectHandler<DynamicUpdater> {
         if(!(e instanceof FileObject o)) return null;
         FileRegistry registry = ctx.getRegistry();
         ItemPredicate predicate = registry.deserializeFromFile(ItemPredicate.class, o.get("predicate"));
-        DynamicItem dynamicItem = registry.deserializeFromFile(BukkitDynamicItem.class, o.get("item"));
+        DynamicItem dynamicItem = registry.deserializeFromFile(DynamicItem.class, o.get("item"));
         if(CruxObjects.checkNull(predicate, dynamicItem)) return null;
         return new CfgDynamicUpdater(predicate, dynamicItem);
     }
