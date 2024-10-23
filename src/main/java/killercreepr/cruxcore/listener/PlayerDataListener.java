dde55c568a6f01670e880d2a209cf97259d2c8b0
KillerCreeper <43390898+KillerCreeper112@users.noreply.github.com>
2024-10-23 15:15:13 -0400

    ye

--- src/main/java/killercreepr/cruxcore/listener/PlayerDataListener.java
+++ src/main/java/killercreepr/cruxcore/listener/PlayerDataListener.java
@@ -3,11 +3,12 @@ package killercreepr.cruxcore.listener;
 import killercreepr.crux.data.entity.PlayerMemory;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class PlayerDataListener implements Listener {
-    @EventHandler(ignoreCancelled = true)
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.LOWEST)
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player p = event.getPlayer();
         PlayerMemory.getOrCreate(p);
