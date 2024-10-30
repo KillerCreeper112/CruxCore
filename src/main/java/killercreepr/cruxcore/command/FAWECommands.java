899a25a2aa01be0dc50db4b6a910f4094b426c75
KillerCreeper <43390898+KillerCreeper112@users.noreply.github.com>
2024-10-30 10:27:18 -0400

    ye

--- src/main/java/killercreepr/cruxcore/command/FAWECommands.java
+++ src/main/java/killercreepr/cruxcore/command/FAWECommands.java
@@ -1,5 +1,8 @@
 package killercreepr.cruxcore.command;
 
+import com.fastasyncworldedit.core.extent.clipboard.ReadOnlyClipboard;
+import com.fastasyncworldedit.core.extent.clipboard.WorldCopyClipboard;
+import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.tree.LiteralCommandNode;
 import com.sk89q.worldedit.LocalSession;
@@ -8,7 +11,11 @@ import com.sk89q.worldedit.bukkit.BukkitAdapter;
 import com.sk89q.worldedit.extension.platform.Actor;
 import com.sk89q.worldedit.extension.platform.permission.ActorSelectorLimits;
 import com.sk89q.worldedit.extent.clipboard.Clipboard;
+import com.sk89q.worldedit.extent.clipboard.io.BuiltInClipboardFormat;
+import com.sk89q.worldedit.extent.clipboard.io.ClipboardWriter;
 import com.sk89q.worldedit.math.BlockVector3;
+import com.sk89q.worldedit.regions.CuboidRegion;
+import com.sk89q.worldedit.regions.Region;
 import com.sk89q.worldedit.session.ClipboardHolder;
 import com.sk89q.worldedit.world.World;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
@@ -21,6 +28,9 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Objects;
 
 public class FAWECommands {
@@ -63,45 +73,59 @@ public class FAWECommands {
                     clipboard.paste(world, placement);
 
                     BlockVector3 pos1 = placement.add(
-                        clipboard.getWidth(), 0, clipboard.getLength()
+                        clipboard.getWidth()/2, 0, clipboard.getLength()/2
                     );
                     BlockVector3 pos2 = placement.add(
-                        clipboard.getWidth() * -1, clipboard.getHeight(), clipboard.getLength() * -1
+                        (clipboard.getWidth()/2) * -1, clipboard.getHeight(), (clipboard.getLength()/2) * -1
                     );
                     session.getRegionSelector(world).selectPrimary(pos1, ActorSelectorLimits.forActor(actor));
                     session.getRegionSelector(world).selectSecondary(pos2, ActorSelectorLimits.forActor(actor));
-
                     sender.sendMessage("Done");
                     return 1;
                 })
-        )/*.then(
+        ).then(
             Commands.literal("save")
-                .executes(ctx ->{
-                    CommandSender sender = getExecutor(ctx.getSource());
-                    if(!(sender instanceof Player p)) return 0;
-                    Actor actor = BukkitAdapter.adapt(p);
-                    LocalSession session = WorldEdit.getInstance().getSessionManager().getIfPresent(actor);
-                    if(session == null){
-                        sender.sendMessage("Session not present.");
-                        return 0;
-                    }
-                    ClipboardHolder holder = session.getClipboard();
-                    Clipboard clipboard = holder.getClipboards().getFirst();
-                    World world = BukkitAdapter.adapt(p.getWorld());
+                .then(
+                    Commands.argument("name", StringArgumentType.string())
+                        .executes(ctx ->{
+                            CommandSender sender = getExecutor(ctx.getSource());
+                            if(!(sender instanceof Player p)) return 0;
+                            String name = ctx.getArgument("name", String.class);
+                            Actor actor = BukkitAdapter.adapt(p);
+                            LocalSession session = WorldEdit.getInstance().getSessionManager().getIfPresent(actor);
+                            if(session == null){
+                                sender.sendMessage("Session not present.");
+                                return 0;
+                            }
+                            World world = BukkitAdapter.adapt(p.getWorld());
+                            try(Clipboard clipboard = ReadOnlyClipboard.of(
+                                WorldEdit.getInstance().newEditSession(world),
+                                new CuboidRegion(session.getRegionSelector(world).getVertices().get(0),session.getRegionSelector(world).getVertices().get(1))
+                            )){
+                                Region region = clipboard.getRegion();
+                                if(!(region instanceof CuboidRegion cuboid)){
+                                    sender.sendMessage("Region not a cuboid.");
+                                    return 0;
+                                }
+                                BlockVector3 origin = getCenterPoint(cuboid.getPos1(), cuboid.getPos2());
+                                int y = Math.min(cuboid.getPos1().y(), cuboid.getPos2().y());
+                                origin = origin.withY(y);
+                                WorldCopyClipboard faweClipboard = new WorldCopyClipboard(() -> clipboard, region);
+                                faweClipboard.setOrigin(origin);
 
-                    Region region = clipboard.getRegion();
-                    if(!(region instanceof CuboidRegion cuboid)){
-                        sender.sendMessage("Reigon not a cuboid.");
-                        return 0;
-                    }
-                    BlockVector3 origin = getCenterPoint(cuboid.getPos1(), cuboid.getPos2());
-                    int y = Math.min(cuboid.getPos1().y(), cuboid.getPos2().y());
-                    origin = origin.withY(y);
-                    WorldCopyClipboard faweClipboard = new WorldCopyClipboard(() -> clipboard, region);
-                    faweClipboard.setOrigin(origin);
-                    return 1;
-                })
-        )*/
+                                File f = WorldEdit.getInstance().getSchematicsFolderPath().resolve( name + ".schem").toFile();
+                                try(ClipboardWriter writer = BuiltInClipboardFormat.FAST.getWriter(new FileOutputStream(f))) {
+                                    writer.writeRotation(BukkitAdapter.adapt(p.getEyeLocation()));
+                                    writer.write(faweClipboard);
+                                    sender.sendMessage("Saved schematic to " + f.getAbsolutePath());
+                                } catch (IOException e) {
+                                    throw new RuntimeException(e);
+                                }
+                            }
+                            return 1;
+                        })
+                )
+        )
         ;
         return dispatcher.build();
     }
