6e687baf6b957990a70b0399ebfa3e3f3204fb45
KillerCreeper <43390898+KillerCreeper112@users.noreply.github.com>
2025-01-13 16:54:12 -0500

    ye

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,21 +16,21 @@ dependencies {
     //Crux Modules
     implementation(files(
         "E:\\Plugins\\Crux2.0\\crux\\CruxMain\\build\\libs\\CruxMain-1.0.jar",
-        "E:\\Plugins\\Crux2.0\\crux\\CruxMenus\\build\\libs\\CruxMenus-1.0-dev.jar",
-        "E:\\Plugins\\Crux2.0\\crux\\CruxPotions\\build\\libs\\CruxPotions-1.0-dev.jar",
-        "E:\\Plugins\\Crux2.0\\crux\\CruxConfigs\\build\\libs\\CruxConfigs-1.0-dev.jar",
-        "E:\\Plugins\\Crux2.0\\crux\\CruxEntities\\build\\libs\\CruxEntities-1.0-dev.jar",
-        "E:\\Plugins\\Crux2.0\\crux\\CruxAttributes\\build\\libs\\CruxAttributes-1.0-dev.jar",
-        "E:\\Plugins\\Crux2.0\\crux\\CruxEnchants\\build\\libs\\CruxEnchants-1.0-dev.jar",
-        "E:\\Plugins\\Crux2.0\\crux\\CruxItems\\build\\libs\\CruxItems-1.0-dev.jar",
-        "E:\\Plugins\\Crux2.0\\crux\\CruxBlocks\\build\\libs\\CruxBlocks-1.0-dev.jar",
-        "E:\\Plugins\\Crux2.0\\crux\\CruxStructures\\build\\libs\\CruxStructures-1.0-dev-all.jar",
-        "E:\\Plugins\\Crux2.0\\crux\\CruxExternal\\build\\libs\\CruxExternal-1.0-dev.jar",
-        "E:\\Plugins\\Crux2.0\\crux\\CruxAdvancements\\build\\libs\\CruxAdvancements-1.0-dev.jar",
+        "E:\\Plugins\\Crux2.0\\crux\\CruxMenus\\build\\libs\\CruxMenus-1.0.jar",
+        "E:\\Plugins\\Crux2.0\\crux\\CruxPotions\\build\\libs\\CruxPotions-1.0.jar",
+        "E:\\Plugins\\Crux2.0\\crux\\CruxConfigs\\build\\libs\\CruxConfigs-1.0.jar",
+        "E:\\Plugins\\Crux2.0\\crux\\CruxEntities\\build\\libs\\CruxEntities-1.0.jar",
+        "E:\\Plugins\\Crux2.0\\crux\\CruxAttributes\\build\\libs\\CruxAttributes-1.0.jar",
+        "E:\\Plugins\\Crux2.0\\crux\\CruxEnchants\\build\\libs\\CruxEnchants-1.0.jar",
+        "E:\\Plugins\\Crux2.0\\crux\\CruxItems\\build\\libs\\CruxItems-1.0.jar",
+        "E:\\Plugins\\Crux2.0\\crux\\CruxBlocks\\build\\libs\\CruxBlocks-1.0.jar",
+        "E:\\Plugins\\Crux2.0\\crux\\CruxStructures\\build\\libs\\CruxStructures-1.0.jar",
+        "E:\\Plugins\\Crux2.0\\crux\\CruxExternal\\build\\libs\\CruxExternal-1.0.jar",
+        "E:\\Plugins\\Crux2.0\\crux\\CruxAdvancements\\build\\libs\\CruxAdvancements-1.0.jar",
         "E:\\Plugins\\Crux2.0\\crux\\CruxGeneration\\build\\libs\\CruxGeneration-1.0.jar",
         "E:\\Plugins\\Crux2.0\\crux\\CruxWorlds\\build\\libs\\CruxWorlds-1.0.jar",
-        "E:\\Plugins\\Crux2.0\\crux\\CruxStats\\build\\libs\\CruxStats-1.0-dev.jar",
-        "E:\\Plugins\\Crux2.0\\crux\\CruxForm\\build\\libs\\CruxForm-1.0-dev.jar",
+        "E:\\Plugins\\Crux2.0\\crux\\CruxStats\\build\\libs\\CruxStats-1.0.jar",
+        "E:\\Plugins\\Crux2.0\\crux\\CruxForm\\build\\libs\\CruxForm-1.0.jar",
     ))
 
     compileOnly(fileTree("libs") {
@@ -40,6 +40,9 @@ dependencies {
 tasks{
     runServer{
         jvmArgs("-Xmx6000M", "-Xms6000M")
+    }
+    assemble{
+        dependsOn(shadowJar)
     }
 }
 
