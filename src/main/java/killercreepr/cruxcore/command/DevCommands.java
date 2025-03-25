37c9e8353df4d30be35399ae578bc888e11cde54
KillerCreeper <43390898+KillerCreeper112@users.noreply.github.com>
2025-03-24 21:42:24 -0400

    ye

--- src/main/java/killercreepr/cruxcore/command/DevCommands.java
+++ src/main/java/killercreepr/cruxcore/command/DevCommands.java
@@ -1,37 +1,27 @@
 package killercreepr.cruxcore.command;
 
-import com.mojang.brigadier.arguments.DoubleArgumentType;
-import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.tree.LiteralCommandNode;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
-import io.papermc.paper.command.brigadier.argument.ArgumentTypes;
-import io.papermc.paper.command.brigadier.argument.resolvers.selector.PlayerSelectorArgumentResolver;
 import io.papermc.paper.plugin.lifecycle.event.LifecycleEventManager;
 import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
-import killercreepr.crux.api.communication.CreateTitle;
-import killercreepr.crux.api.plugin.module.CruxModule;
-import killercreepr.crux.api.text.tags.container.TagContainer;
 import killercreepr.crux.core.Crux;
-import killercreepr.crux.core.command.argument.CruxCmdArguments;
 import killercreepr.crux.core.data.util.MapBuilder;
 import killercreepr.crux.core.plugin.CruxPlugin;
-import killercreepr.crux.core.util.CruxMath;
-import killercreepr.crux.core.util.CruxString;
 import killercreepr.cruxconfig.config.bukkit.file.CruxJson;
 import killercreepr.cruxconfig.config.common.element.FileArray;
 import killercreepr.cruxconfig.config.common.element.FileObject;
 import killercreepr.cruxcore.CruxCore;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.event.ClickEvent;
-import net.kyori.adventure.text.event.HoverEvent;
-import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
 public class DevCommands {
@@ -54,6 +44,9 @@ public class DevCommands {
     private final Map<String, Generation> generations = new MapBuilder<String, Generation>()
         .put("generated", new Generated())
         .put("handheld", new HandHeld())
+        .put("bow", new Bow())
+        .put("crossbowbow", new Crossbow())
+        .put("fishing_rod", new FishingRod())
         .build();
     public LiteralCommandNode<CommandSourceStack> build(LiteralArgumentBuilder<CommandSourceStack> dispatcher,
                                                                   LifecycleEventManager<?> manager){
@@ -63,13 +56,7 @@ public class DevCommands {
                 .then(
                     Commands.argument("type", StringArgumentType.string())
                         .suggests((d, e) ->{
-                            List.of(
-                                "handheld",
-                                "bow",
-                                "crossbow",
-                                "fishing_rod",
-                                "generated"
-                            ).forEach(e::suggest);
+                            generations.keySet().forEach(e::suggest);
                             return e.buildFuture();
                         })
                         .then(
@@ -186,6 +173,238 @@ public class DevCommands {
             json.serialize("model", new FileObject().addProperty("type", "model")
                 .addProperty("model", Crux.key(name).asString()));
             json.save();
+        }
+    }
+
+    private static class Bow extends Generation{
+        @Override
+        public void generate(String name) {
+            //{
+            //  "parent": "minecraft:item/bow",
+            //  "textures": {
+            //    "layer0": "crux:item/ice_bow_pulling_1"
+            //  }
+            //}
+            CruxJson json = new CruxJson(CruxCore.core(), "dev/generated/models/" + parseName(name));
+            json.serialize("parent", "minecraft:item/bow");
+            json.serialize("textures", new FileObject().addProperty("layer0", Crux.key(name).asString()));
+            json.save();
+
+            for(int i = 0; i < 3; i++){
+                json = new CruxJson(CruxCore.core(), "dev/generated/models/" + parseName(name) + "_pulling_" + i);
+                json.serialize("parent", "minecraft:item/bow");
+                json.serialize("textures", new FileObject().addProperty("layer0", Crux.key(name + "_pulling_" + i).asString()));
+                json.save();
+            }
+            String pulling0 = Crux.key(name) + "_pulling_0";
+            String pulling1 = Crux.key(name) + "_pulling_1";
+            String pulling2 = Crux.key(name) + "_pulling_2";
+            String jsonText = String.format("""
+        {
+          "model": {
+            "type": "minecraft:condition",
+            "on_false": {
+              "type": "minecraft:model",
+              "model": "crux:item/%s"
+            },
+            "on_true": {
+              "type": "minecraft:range_dispatch",
+              "entries": [
+                {
+                  "model": {
+                    "type": "minecraft:model",
+                    "model": "%s"
+                  },
+                  "threshold": 0.65
+                },
+                {
+                  "model": {
+                    "type": "minecraft:model",
+                    "model": "%s"
+                  },
+                  "threshold": 0.9
+                }
+              ],
+              "fallback": {
+                "type": "minecraft:model",
+                "model": "%s"
+              },
+              "property": "minecraft:use_duration",
+              "scale": 0.05
+            },
+            "property": "minecraft:using_item"
+          }
+        }
+        """, Crux.key(name).asString(), pulling1, pulling2, pulling0);
+
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+            try {
+                // Ensure the directory exists
+                File file = new CruxJson(CruxCore.core(), "dev/generated/items/" + parseName(name), false).file();
+                file.getParentFile().mkdirs();
+
+                // Write to the file
+                try (FileWriter writer = new FileWriter(file)) {
+                    gson.toJson(gson.fromJson(jsonText, Object.class), writer);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static class Crossbow extends Generation{
+        @Override
+        public void generate(String name) {
+            //{
+            //  "parent": "minecraft:item/crossbow",
+            //  "textures": {
+            //    "layer0": "crux:item/ancient_crossbow_firework"
+            //  }
+            //}
+            CruxJson json = new CruxJson(CruxCore.core(), "dev/generated/models/" + parseName(name));
+            json.serialize("parent", "minecraft:item/crossbow");
+            json.serialize("textures", new FileObject().addProperty("layer0", Crux.key(name).asString() + "_standby"));
+            json.save();
+            json = new CruxJson(CruxCore.core(), "dev/generated/models/" + parseName(name) + "_arrow");
+            json.serialize("parent", "minecraft:item/crossbow");
+            json.serialize("textures", new FileObject().addProperty("layer0", Crux.key(name).asString() + "_arrow"));
+            json.save();
+            json = new CruxJson(CruxCore.core(), "dev/generated/models/" + parseName(name) + "_firework");
+            json.serialize("parent", "minecraft:item/crossbow");
+            json.serialize("textures", new FileObject().addProperty("layer0", Crux.key(name).asString() + "_firework"));
+            json.save();
+
+            for(int i = 0; i < 3; i++){
+                json = new CruxJson(CruxCore.core(), "dev/generated/models/" + parseName(name) + "_pulling_" + i);
+                json.serialize("parent", "minecraft:item/crossbow");
+                json.serialize("textures", new FileObject().addProperty("layer0", Crux.key(name).asString() + "_pulling_" + i));
+                json.save();
+            }
+            String arrow = Crux.key(name) + "_arrow";
+            String firework = Crux.key(name) + "_firework";
+            //name
+            String pulling1 = Crux.key(name) + "_pulling_1";
+            String pulling2 = Crux.key(name) + "_pulling_2";
+            String pulling0 = Crux.key(name) + "_pulling_0";
+            String jsonText = String.format("""
+                {
+                  "model": {
+                    "type": "minecraft:condition",
+                    "on_false": {
+                      "type": "minecraft:select",
+                      "cases": [
+                        {
+                          "model": {
+                            "type": "minecraft:model",
+                            "model": "%s"
+                          },
+                          "when": "arrow"
+                        },
+                        {
+                          "model": {
+                            "type": "minecraft:model",
+                            "model": "%s"
+                          },
+                          "when": "rocket"
+                        }
+                      ],
+                      "fallback": {
+                        "type": "minecraft:model",
+                        "model": "%s"
+                      },
+                      "property": "minecraft:charge_type"
+                    },
+                    "on_true": {
+                      "type": "minecraft:range_dispatch",
+                      "entries": [
+                        {
+                          "model": {
+                            "type": "minecraft:model",
+                            "model": "%s"
+                          },
+                          "threshold": 0.58
+                        },
+                        {
+                          "model": {
+                            "type": "minecraft:model",
+                            "model": "%s"
+                          },
+                          "threshold": 1.0
+                        }
+                      ],
+                      "fallback": {
+                        "type": "minecraft:model",
+                        "model": "%s"
+                      },
+                      "property": "minecraft:crossbow/pull"
+                    },
+                    "property": "minecraft:using_item"
+                  }
+                }
+        """, arrow, firework, Crux.key(name).asString(), pulling1, pulling2, pulling0);
+
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+            try {
+                // Ensure the directory exists
+                File file = new CruxJson(CruxCore.core(), "dev/generated/items/" + parseName(name), false).file();
+                file.getParentFile().mkdirs();
+
+                // Write to the file
+                try (FileWriter writer = new FileWriter(file)) {
+                    gson.toJson(gson.fromJson(jsonText, Object.class), writer);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    private static class FishingRod extends Generation{
+        @Override
+        public void generate(String name) {
+            CruxJson json = new CruxJson(CruxCore.core(), "dev/generated/models/" + parseName(name));
+            json.serialize("parent", "minecraft:item/handheld_rod");
+            json.serialize("textures", new FileObject().addProperty("layer0", Crux.key(name).asString()));
+            json.save();
+
+            json = new CruxJson(CruxCore.core(), "dev/generated/models/" + parseName(name) + "_cast");
+            json.serialize("parent", "minecraft:item/handheld_rod");
+            json.serialize("textures", new FileObject().addProperty("layer0", Crux.key(name).asString() + "_cast"));
+            json.save();
+
+            String jsonText = String.format("""
+                {
+                          "model": {
+                            "type": "minecraft:condition",
+                            "on_false": {
+                              "type": "minecraft:model",
+                              "model": "%s"
+                            },
+                            "on_true": {
+                              "type": "minecraft:model",
+                              "model": "%s"
+                            },
+                            "property": "minecraft:fishing_rod/cast"
+                          }
+                        }
+        """, Crux.key(name).asString(), Crux.key(name) + "_cast");
+
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+            try {
+                // Ensure the directory exists
+                File file = new CruxJson(CruxCore.core(), "dev/generated/items/" + parseName(name), false).file();
+                file.getParentFile().mkdirs();
+
+                // Write to the file
+                try (FileWriter writer = new FileWriter(file)) {
+                    gson.toJson(gson.fromJson(jsonText, Object.class), writer);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
